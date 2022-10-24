d41d1c307754d44762ee00c9375425903b413d3e
Олексій Демидов <orange.family.cn@gmail.com>
2022-10-24 21:34:23 +0300

    Create DataToBear.js

---
+++ src/components/comp/Kitchen/Data/DataToBear.js
@@ -0,0 +1,27 @@
+const ToBearMass=[
+    {
+        id:1,
+        title:"вушки хрюшки 150г",
+        price:"65 грн",
+        src: require('../img/asorti/meattarelka.png'),
+        text:"",
+    },
+    {
+        id:2,
+        title:"сет «від кусто» 270г",
+        price:"150 грн",
+        src:require('../img/asorti/tartobear.png'),
+        text:"янтарна путасу з перцем, рибка ліпаріс з перцем, ікряники, кальмари, чіпси з лавашу",
+    },
+    
+    {
+        id:3,
+        title:"сет до пива 320г",
+        price:"150 грн",
+        src:require('../img/asorti/tovodka.png'),
+        text:"бастурма, хамон, снеки кабаноси, сир косичка, чіпси з лаваша, горіхи",
+    },
+        
+]
+
+export default ToBearMass;
